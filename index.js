48242a1cbcc47eba8bc9e241ea1a4e7d28ebf620
lemonsprite <alfath.noor17@gmail.com>
2022-03-25 00:08:50 +0700

    external data generate

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const data = [
-    {width: 200, height: 100, fill: 'red'},
-    {width: 100, height: 60, fill: 'white'},
-    {width: 50, height: 30, fill: 'blue'},
-]
+// Select element
+const svg = d3.select("svg");
 
-const svg = d3.select('svg')
+d3.json("planets.json").then((data) => {
+  const circle = svg.selectAll("circle").data(data);
 
-// Join data to rect
-const rect = svg.selectAll('rect')
-.data(data)
+  // add attr to circle
+  circle
+    .attr("cy", 400)
+    .attr("cx", (d) => d.distance)
+    .attr("r", (d) => d.radius)
+    .attr("fill", (d) => d.fill);
 
-//  Add attr to rect already un dom
-rect.attr('width', (d,i,n) => d.width)
-.attr('height', d => d.height)
-.attr('fill', d => d.fill)
-
-// Generate rect depends on data
-rect.enter()
-.append('rect')
-.attr('width', (d,i,n) => d.width)
-.attr('height', d => d.height)
-.attr('fill', d => d.fill)
-
-
-// Arrow Function refer to window element
-// Arrow Function refer to element that selected
+  circle
+    .enter()
+    .append("circle")
+    .attr("cy", 200)
+    .attr("cx", (d) => d.distance)
+    .attr("r", (d) => d.radius)
+    .attr("fill", (d) => d.fill);
+});
