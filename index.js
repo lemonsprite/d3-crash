5d0ed29cce7ef9db5c6a230cf041abae64d7bb48
lemonsprite <alfath.noor17@gmail.com>
2022-03-24 23:55:50 +0700

    enter method

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ const data = [
 
 const svg = d3.select('svg')
 
+// Join data to rect
 const rect = svg.selectAll('rect')
 .data(data)
+
+//  Add attr to rect already un dom
+rect.attr('width', (d,i,n) => d.width)
+.attr('height', d => d.height)
+.attr('fill', d => d.fill)
+
+// Generate rect depends on data
+rect.enter()
+.append('rect')
 .attr('width', (d,i,n) => d.width)
 .attr('height', d => d.height)
 .attr('fill', d => d.fill)
