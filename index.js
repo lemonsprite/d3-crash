914f917cffdb6314102c1c1e3d5c1aa81e7e90df
lemonsprite <alfath.noor17@gmail.com>
2022-03-25 00:45:16 +0700

    min, max, extent method

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 const svg = d3.select("svg");
 
 d3.json("menu.json").then((data) => {
+  
+  // Get minimum value in data
+  // const min = d3.min(data, d => d.orders)
+  // const max = d3.max(data, d => d.orders)
+  // const extent = d3.extent(data, d => d.orders)
+  
   // Linear Scale Declare
-  const y = d3.scaleLinear().domain([0, 1000]).range([0, 500]);
+  const y = d3.scaleLinear().domain([0, d3.max(data, d => d.orders)]).range([0, 500]);
 
   // Band Scale Declare
   const x = d3
