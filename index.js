4d407033f9ed1c816f4ec0ec05927a57d786c697
lemonsprite <alfath.noor17@gmail.com>
2022-03-28 12:34:13 +0700

    transition

--- index.js
+++ index.js
@@ -28,54 +28,83 @@ const xAxisGroup = graph
 
 const yAxisGroup = graph.append("g");
 
-d3.json("menu.json").then((data) => {
-  // Get minimum value in data
-  // const min = d3.min(data, d => d.orders)
-  // const max = d3.max(data, d => d.orders)
-  // const extent = d3.extent(data, d => d.orders)
+// scales
+const x = d3.scaleBand().range([0, 500]).paddingInner(0.2).paddingOuter(0.2);
+const y = d3.scaleLinear().range([graphHeight, 0]);
 
-  // Linear Scale Declare
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.orders)])
-    .range([graphHeight, 0]);
+// create and Calls Axs
+const xAxis = d3.axisBottom(x);
+const yAxis = d3
+  .axisLeft(y)
+  .ticks(8)
+  .tickFormat((d) => d + " orders");
 
-  // Band Scale Declare
-  const x = d3
-    .scaleBand()
-    .domain(data.map((i) => i.name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+// Update Xaxis Text
+xAxisGroup
+  .selectAll("text")
+  .attr("transform", "rotate(-40)")
+  .attr("text-anchor", "end")
+  .attr("fill", "pink");
 
-  // join data to rect
+// Algoritma Update Chart / Realtime Chart
+const update = (data) => {
+  // 1. Update Scale & Domain
+  y.domain([0, d3.max(data, (d) => d.orders)]);
+  x.domain(data.map((i) => i.name));
+
+  // 2. Join data to element
   const rects = graph.selectAll("rect").data(data);
 
+  // 3. Remove unwanted shape
+  rects.exit().remove();
+
+  // 4. Update current shape in DOM
   rects
     .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
     .attr("fill", "orange")
-    .attr("x", (d) => x(d.name));
+    .attr("x", (d) => x(d.name))
+    .transition().duration(500)
+      .attr("height", (d) => graphHeight - y(d.orders))
+      .attr("y", (d) => y(d.orders));
 
   rects
     .enter()
     .append("rect")
     .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
+    .attr("height", 0)
     .attr("fill", "orange")
     .attr("x", (d) => x(d.name))
-    .attr('y', d => y(d.orders))
+    .attr("y", graphHeight)
+    .transition().duration(500)
+      .attr("y", (d) => y(d.orders))
+      .attr("height", (d) => graphHeight - y(d.orders));
 
-  // create and Calls Axs
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y).ticks(8).tickFormat(d => d + ' orders')
-
+  // Call axis
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
+};
+var data = [];
+db.collection("dishes").onSnapshot((res) => {
+  res.docChanges().forEach((d) => {
+    console.log(d);
+    const doc = { ...d.doc.data(), id: d.doc.id };
 
-  xAxisGroup.selectAll('text')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
-    .attr('fill', "pink")
-    
+    switch (d.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const i = data.findIndex((x) => x.id == doc.id);
+        data[x] = doc;
+        break;
+      case "removed":
+        data = data.filter((x) => x.id !== doc.id);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  update(data);
 });
